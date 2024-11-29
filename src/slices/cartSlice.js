f7740cedd6bd6ca762aa047209af1b52758347f3
pratheesh18 <pratheesh1218@gmail.com>
2024-11-29 11:31:49 +0530

    updated the cart slice to persist the cart item in local storage

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -1,28 +1,34 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialCartState = JSON.parse(localStorage.getItem('cart')) || [];
 
 const cartSlice = createSlice({
-    name : 'cart',
-    initialState : [],
-    reducers : {
-        addToCart(state,action){
-            const existingItem = state.find(item => item.id === action.payload.id);
-            if(existingItem){
-                existingItem.quantity += 1;
-            }else{
-                state.push({...action.payload,quantity:1});
-            }
-        },
-        removeFromCart(state,action){
-            return state.filter(item => item.id !== action.payload);
-        },
-        updateQuantity(state,action) {
-            const item = state.find(item => item.id === AuthenticatorAssertionResponse.payload.id);
-            if(item){
-                item.quantity = action.payload.quantity;
-            }
-        }
-    }
+  name: 'cart',
+  initialState: initialCartState,
+  reducers: {
+    addToCart(state, action) {
+      const existingItem = state.find((item) => item.id === action.payload.id);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.push({ ...action.payload, quantity: 1 });
+      }
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
+    removeFromCart(state, action) {
+      const updatedCart = state.filter((item) => item.id !== action.payload);
+      localStorage.setItem('cart', JSON.stringify(updatedCart));
+      return updatedCart;
+    },
+    updateQuantity(state, action) {
+      const item = state.find((item) => item.id === action.payload.id);
+      if (item) {
+        item.quantity = action.payload.quantity;
+      }
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
+  },
 });
 
-export const {addToCart,removeFromCart,updateQuantity} =  cartSlice.actions;
+export const { addToCart, removeFromCart, updateQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
